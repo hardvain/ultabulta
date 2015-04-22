9a58a57d309a8e3172de96af07bd127556d839cb
Aravindh S <mail@aravindh.io>
2015-04-22 19:33:58 +0530

    Added edit & delete options

--- public/app/scripts/controllers/words/WordController.js
+++ public/app/scripts/controllers/words/WordController.js
@@ -9,4 +9,12 @@ app.controller('WordController', function ($scope, $timeout, $q, $stateParams, $
   $scope.nextWord = function () {
     $state.go("app.words.word", {id: parseInt($stateParams.id) + 1})
   };
+
+  $scope.delete = function(id,type){
+    console.log('deleting '+ id +' type: '+type);
+  };
+
+  $scope.edit = function(id,type){
+    console.log('editing '+ id +' type: '+type);
+  }
 });
